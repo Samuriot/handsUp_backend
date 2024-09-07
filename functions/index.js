6129e0ade9195b431ac4cf79901905ad778d517b
Samuriot <92127962+Samuriot@users.noreply.github.com>
2024-09-06 17:08:55 -0700

    pushing into new repo

---
+++ functions/index.js
@@ -0,0 +1,67 @@
+/* eslint-disable indent */
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://handsup-eb897-default-rtdb.firebaseio.com",
+});
+
+const express = require("express");
+const cors = require("cors");
+const db = admin.firestore();
+
+const app = express();
+app.use(cors({origin: true}));
+
+// Routes
+app.get("/", (req, res) => {
+    return res.status(200).send("Hello World!");
+});
+
+// Create
+// Post
+app.post("/api/create", (req, res) => {
+    (async () => {
+        try {
+            await db.collection("concerts").doc("/" + req.body.id + "/")
+            .create({
+                artist: req.body.artist,
+                data: req.body.data,
+                location: req.body.location,
+                picture: req.body.picture,
+            });
+            return res.status(200).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// Read a specific concert based on ID
+// Get
+app.get("/api/read/:id", (req, res) => {
+    (async () => {
+        try {
+            const document = db.collection("concerts").doc(req.params.id);
+            let product = await document.get();
+            let response = product.data();
+
+            return res.status(200).send(response);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
+
+// Update
+// Put
+
+// Delete
+// Delete
+
+exports.app = functions.https.onRequest(app);
